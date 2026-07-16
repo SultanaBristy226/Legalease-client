4b2c01ea1b4ae5e6a590edd73441594f660e32b6
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-07-16 21:47:17 +0600

    add

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,23 @@
 import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
+import { MongoClient } from "mongodb";
+
+// MongoDB Client
+const client = new MongoClient(process.env.MONGODB_URI!);
+const db = client.db();
 
 export const auth = betterAuth({
-  database: {
-    type: "mongodb",
-    url: process.env.NEXT_PUBLIC_MONGODB_URI!,
+  database: mongodbAdapter(db, {
+    client: client,
+  }),
+  secret: process.env.BETTER_AUTH_SECRET!,
+  emailAndPassword: {
+    enabled: true,
   },
   socialProviders: {
     google: {
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
-});
+});cd
