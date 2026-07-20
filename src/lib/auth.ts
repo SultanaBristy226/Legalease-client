69cd40d5515f876ebb5ea8496701d3b51b575cd4
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-07-20 20:16:54 +0600

    add better auth secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,18 +2,13 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { MongoClient } from "mongodb";
 
-// MongoDB Client
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    client: client,
-  }),
+  database: mongodbAdapter(db, { client }),
   secret: process.env.BETTER_AUTH_SECRET!,
-  emailAndPassword: {
-    enabled: true,
-  },
+  emailAndPassword: { enabled: true },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
